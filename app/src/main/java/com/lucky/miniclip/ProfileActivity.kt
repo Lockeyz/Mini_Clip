391f493b683148b848ddccb59fea87cf95b3941e
ducloc <locdivine@gmail.com>
2024-03-01 14:12:57 +0700

    design_profile_screen

---
+++ app/src/main/java/com/lucky/miniclip/ProfileActivity.kt
@@ -0,0 +1,73 @@
+package com.lucky.miniclip
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.lucky.miniclip.databinding.ActivityProfileBinding
+import com.lucky.miniclip.model.UserModel
+
+class ProfileActivity : AppCompatActivity() {
+    lateinit var binding: ActivityProfileBinding
+    lateinit var profileUserId : String
+    lateinit var currentUserId : String
+
+    lateinit var profileUserModel : UserModel
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding  = ActivityProfileBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        profileUserId = intent.getStringExtra("profile_user_id")!!
+        currentUserId =  FirebaseAuth.getInstance().currentUser?.uid!!
+
+        if(profileUserId==currentUserId){
+            //Current user profile
+            binding.profileBtn.text = "Logout"
+            binding.profileBtn.setOnClickListener {
+                logout()
+            }
+        }else{
+            //other user profile
+        }
+        getProfileDataFromFirebase()
+    }
+
+    fun logout(){
+        FirebaseAuth.getInstance().signOut()
+        val intent = Intent(this,LoginActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        startActivity(intent)
+    }
+
+    fun getProfileDataFromFirebase(){
+        Firebase.firestore.collection("users")
+            .document(profileUserId)
+            .get()
+            .addOnSuccessListener {
+                profileUserModel = it.toObject(UserModel::class.java)!!
+                setUI()
+            }
+    }
+
+    fun setUI(){
+        profileUserModel.apply {
+            Glide.with(binding.profilePic).load(profilePic)
+                .apply(RequestOptions().placeholder(R.drawable.icon_account_circle))
+                .into(binding.profilePic)
+            binding.profileUsername.text ="@"+ userName
+            binding.progressBar.visibility = View.INVISIBLE
+            binding.followingCount.text = followingList.size.toString()
+            binding.followerCount.text = followerList.size.toString()
+            Firebase.firestore.collection("videos")
+                .whereEqualTo("uploaderId",profileUserId)
+                .get().addOnSuccessListener {
+                    binding.postCount.text  = it.size().toString()
+                }
+        }
+    }
+}
